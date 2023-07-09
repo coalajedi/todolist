b9bff1b66540a7f59a5edb4286a6fb0c91e7c4f1
Felipe Duarte <coalajedi.ios@gmail.com>
2023-07-09 01:04:42 -0300

    style: :lipstick: add a theme file to the app
    This commit just add a file with a theme for the app according to the app style guide
    
    Signed-off-by: Felipe Duarte <coalajedi.ios@gmail.com>

---
+++ src/theme/app-theme.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2023 Felipe D. Silva (a.k.a Coala Jedi)
+ * All Rights Reserved
+ *
+ * Unauthorized copying or redistribution of this file in source and binary forms via any medium
+ * is strictly prohibited.
+ */
+
+export const appTheme = {
+  colors: {
+    base: {
+      gray100: '#F2F2F2',
+      gray200: '#D9D9D9',
+      gray300: '#808080',
+      gray400: '#333333',
+      gray500: '#262626',
+      gray600: '#1A1A1A',
+      gray700: '#0D0D0D',
+    },
+    product: {
+      blue: '#4EA8DE',
+      blueDark: '#1E6F9F',
+      purple: '#8284FA',
+      purpleDark: '#5E60CE',
+    },
+    feedback: {
+      danger: '#E25858',
+    },
+  },
+
+  typography: {
+    fontFamily: 'Inter',
+    sizes: {
+      small: 12,
+      medium: 14,
+      large: 16,
+    },
+    lineHeight: '140%',
+    weights: {
+      regular: 400,
+      bold: 700,
+    },
+  },
+};
