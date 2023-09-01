3d9bf2f1dbe5a0ff79c210fc52e33972390b4075
Felipe Duarte <coalajedi.ios@gmail.com>
2023-09-01 13:47:16 -0300

    style: :wheelchair: implement StatusBar component in entire app
    This commit adds the StatusBar component in the App component to improve accessibility
    
    Signed-off-by: Felipe Duarte <coalajedi.ios@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
+/*
+ * Copyright (C) 2023 Felipe D. Silva (a.k.a Coala Jedi)
+ * All Rights Reserved
+ *
+ * Unauthorized copying or redistribution of this file in source and binary forms via any medium
+ * is strictly prohibited.
+ */
+
 import React from 'react';
 import {Home} from './src/screens/Home';
+import {StatusBar} from 'react-native';
 
 export function App() {
   return (
     <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
       <Home />
     </>
   );
